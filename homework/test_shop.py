caf80c4d9216106c6de719790af68dbb71e0539c
Dmitriy Vasilyev <61234128+d-svtch@users.noreply.github.com>
2025-03-04 13:48:04 +0300

    separated the tests

--- homework/test_shop.py
+++ homework/test_shop.py
@@ -10,6 +10,7 @@ from homework.models import Product, Cart
 def product():
     return Product("book", 100, "This is a book", 1000)
 
+
 @pytest.fixture
 def new_product():
     return Product("postcard", 10, "Just a postcard", 5000)
@@ -55,22 +56,30 @@ class TestCart:
         Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
     """
 
-    def test_add_product(self, cart, product, new_product):
+    def test_add_product_with_quantity(self, cart, product):
         cart.add_product(product, buy_count=2)
         assert cart.products[product] == 2
+
+    def test_add_product_without_quantity(self, cart, product):
         cart.add_product(product)
-        assert cart.products[product] == 3
+        assert cart.products[product] == 1
+
+    def test_add_few_different_products(self, cart, product, new_product):
+        cart.add_product(product, buy_count=2)
         cart.add_product(new_product, buy_count=5)
+        assert cart.products[product] == 2
         assert cart.products[new_product] == 5
 
-    def test_remove_product(self, cart, product):
+    def test_remove_few_products(self, cart, product):
         cart.add_product(product, buy_count=10)
         cart.remove_product(product, remove_count=5)
         assert cart.products[product] == 5
 
+    def test_remove_all_products(self, cart, product):
         cart.remove_product(product)
         assert product is not cart.products
 
+    def test_more_products_then_in_cart(self, cart, product):
         cart.add_product(product, buy_count=10)
         cart.remove_product(product, remove_count=15)
         assert product is not cart.products
@@ -82,16 +91,19 @@ class TestCart:
         assert cart.products == {}
 
     def test_get_total_price(self, cart, product, new_product):
-        cart.add_product(product, buy_count=2)
-        cart.add_product(new_product, buy_count=3)
-        assert cart.get_total_price() == 100 * 2 + 10 * 3
+        temp_count_1 = 2
+        temp_count_2 = 3
+        cart.add_product(product, buy_count=temp_count_1)
+        cart.add_product(new_product, buy_count=temp_count_2)
+        assert cart.get_total_price() == product.price * temp_count_1 + new_product.price * temp_count_2
 
-    def test_buy(self, cart, product, new_product):
+    def test_successfull_buy(self, cart, product, new_product):
         cart.add_product(product, buy_count=200)
         cart.add_product(new_product, buy_count=1000)
         assert cart.buy() == "Покупка успешно завершена!"
         assert product.quantity == 800
         assert new_product.quantity == 4000
 
+    def test_unsuccessfull_buy(self, cart, product, new_product):
         cart.add_product(product, buy_count=50000)
         assert cart.buy() is ValueError
